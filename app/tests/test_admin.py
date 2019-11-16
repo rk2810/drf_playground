51d1959bbbe3b238e07f596ce02f9121ce788799
rk2810 <rohan.kumquat@gmail.com>
2019-11-16 16:10:42 +0530

    Admin: Add custom fieldset to admin page to support our custom model. - Add test for it as well.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- app/tests/test_admin.py
+++ app/tests/test_admin.py
@@ -26,3 +26,10 @@ class AdminTests(TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """the user edit page"""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)

aba9bebf451b14b292f19c0df6a1ca1294a9ad48
rk2810 <rohan.kumquat@gmail.com>
2019-11-24 19:19:43 +0530

    Recipe: create list Tag APIs with test cases.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

---
+++ app/recipe/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+from recipe import views
+
+router = DefaultRouter()
+router.register("tags", views.TagViewSet)
+
+app_name = "recipe"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
